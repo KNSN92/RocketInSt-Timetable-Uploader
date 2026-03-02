495f57310986718c9e1036001d3c8b5f0f29f4cc
KNSN92 <kekenshin92@gmail.com>
2026-03-02 11:33:12 +0900

    add: wip streamlitとocr

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import requests
+import json
+import streamlit as st
+import pandas as pd
+import ocr
 
 RocketInStToken = os.getenv("RocketInStToken")
+url = "https://rocket-in-st.vercel.app/api/timetable"
 
-def main():
-    pass
+# webui
+async def main():
+    st.write("# uploader")
+    uploaded_img = st.file_uploader("Upload", type=["png"])
+    if uploaded_img:
+        st.image(uploaded_img)
+        test = os.path.join("./TimeTableImages", uploaded_img.name)
+        with open(test, "wb") as f:
+            f.write(uploaded_img.read())
+            print("画像保存")
+        print("ocr開始")
+        df = await ocr.ocr(test)
+        print("ocr終了")
+
 
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
+
+def upload_time_table():
+    headers = {
+        'Authorization': f'Bearer {RocketInStToken}'
+    }
+    timetable = df.to_dict()
+    for room, lessons in timetable.items():
+        timetable[room] = {
+            period : title for period, title in lessons.items() if title is not None
+        }
+    timetable_json = json.dumps(timetable, ensure_ascii=False)
+    print(timetable_json)
+    res = requests.post(url, headers=headers, data=timetable_json)
+    print(res)
---
+++ ocr.py
@@ -0,0 +1,120 @@
+# yomitokuと前処理とかのライブラリのインポート
+from yomitoku import DocumentAnalyzer
+from yomitoku.export.export_csv import table_to_csv
+from yomitoku.data.functions import load_image
+from yomitoku.utils.visualizer import reading_order_visualizer
+import numpy as np
+import torch
+import cv2
+from multiprocessing import freeze_support
+
+# その他のライブラリのインポート
+import time
+from pprint import pprint
+from IPython.display import display
+from PIL import Image
+import pandas as pd
+import json
+# yomitokuがマルチプロセスを使う為、freeze_supportを呼び出す
+freeze_support()
+
+# 変数の設定
+# 下記のパスを書き換えると読み取る画像を変更できる
+# img_path = "./images/image7.png"
+# dict名 = {'修正前1': '修正後1', '修正前2': '修正後2'}
+# 読み取りミス修正用dict
+miss = {
+    'Na': 'Nα','Np': 'Nβ', 'Nv': 'Nγ'
+    }
+# 時間
+period_ja_to_en = {
+    "朝": "Morning",
+    "朝礼": "MorningMeeting",
+    "1限": "FirstPeriod",
+    "2限": "SecondPeriod",
+    "3限": "ThirdPeriod",
+    "昼休み": "NoonRecess",
+    "4限": "FourthPeriod",
+    "5限": "FifthPeriod",
+    "6限": "SixthPeriod",
+    "終礼": "ClosingMeeting",
+    "放課後": "AfterSchool",
+}
+# 部屋名と時間割名の定数を定義
+ROOM_NAMES = ["大広間", "秘密基地", "万里", "7階"]
+TIME_SLOTS = ["朝", "朝礼", "1限", "2限", "3限", "昼休み", "4限", "5限", "6限", "終礼", "放課後"]
+
+# デバイスの設定
+# MPSはyomitokuのバグで使えないけどgithubでプルリクが承認されたので、近々使えるようになるはず。
+# 簡単な修正だからpatchを適用しても良いけど面倒いからね〜
+async def ocr(img_path: str):
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+    print(f"Using device: {device}")
+
+    # 画像を読み取ってグレースケールに変換
+    img = load_image(img_path)
+
+    img = np.array(img)
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
+    img = img_gray
+    # OCRの実行関数の定義
+    # yomitokuのanalyzerは__call__で実行出来るけど内部でasyncio.runを使っているので、jupyter notebook上で直接実行できない。
+    # なので、async関数を定義してから実行する形にする。
+
+    async def analyze(img: np.ndarray):
+        analyzer = DocumentAnalyzer(device=device, visualize=True, configs={})
+        analyzer.img = img
+        resutls, ocr, layout = await analyzer.run(img)
+
+        if analyzer.visualize:
+            layout = reading_order_visualizer(layout, resutls)
+
+        return resutls, ocr, layout
+    # OCRの実行
+    start = time.time()
+    results, img, layout = await analyze(img)
+    end = time.time()
+    print(f"OCR Time: {end - start} seconds")
+
+
+    # テーブルの中身が空だったとき用？
+    if len(results.tables) == 0:
+        print("No tables found.")
+    table = table_to_csv(results.tables[0], ignore_line_break=False)
+    # pprint(table)
+    df = pd.DataFrame(table)
+
+    df = df[df[:] != ""].dropna(how="all")
+    df = df.drop(1, axis=1).drop(0, axis=0)
+    if df.shape[1] != len(ROOM_NAMES)+1:
+        print("Table format is unexpected. Trying to adjust...")
+        df = df.iloc[:, :len(ROOM_NAMES)+1]
+
+    df.columns=["時間"] + ROOM_NAMES
+    df = df.set_index("時間")
+    df = df.replace("\n", " ", regex=True)
+
+    # 上記のdictを使用した読み取りミスの修正
+    for k, v in miss.items():
+        df = df.replace(k,v, regex=True)
+    # 時間を変換
+    df = df.rename(index=period_ja_to_en)
+    # 重複を削除
+    df = df[~df.index.duplicated()]
+    # nanをNoneにする
+    df = df.replace([np.nan], [None])
+    # jsonにアウトプット
+    df.to_json("output.json", force_ascii=False)
+    return df
+
+
+if __name__=="__main__":
+    import asyncio
+    asyncio.run(main())
